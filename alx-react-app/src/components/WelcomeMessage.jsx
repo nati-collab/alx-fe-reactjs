23b4a8c04a4dd591699b246aac56887763737b81
nati <girma7575@gmail.com>
2025-07-16 16:09:20 +0300

    Add Header, MainContent, and Footer components to App

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,11 +1,15 @@
-function WelcomeMessage() {
-    return (
-        <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
-        </div>
-    );
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
 }
 
-export default WelcomeMessage;
+export default App;
